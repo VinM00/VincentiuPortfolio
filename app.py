1b228e3b915af6d498cead13d11f0c70d07b6efa
vin <vincentiu.mocanu00@gmail.com>
2023-12-12 22:07:51 +0000

    new files

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, jsonify
+"""This part of the script handles Flask servers and renders the portfolio page"""
+from flask import Flask, render_template, request
+import datetime as dt
+
 
 app=Flask(__name__)
 
-    
+   
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+  "read home.html template"
+  return render_template("home.html", year=dt.datetime.now().year)
 
-@app.route("/aboutme")
-def about():
-    return render_template("aboutme.html")
+@app.route("/projects")
+def portfolio():
+  return render_template("projects.html")
+
 
 if __name__== "__main__":
-    app.run(debug=True)
-
+  app.run(debug=True)
